ae1578e3460c016a39bfebe6ddb218d2ffe9dfeb
ShreeRaz <ankitt60raz@gmail.com>
2025-12-30 20:40:05 +0545

    Add professional UI styling for Expense Tracker

---
+++ expense-tracker/src/components/Incometracker.jsx
@@ -0,0 +1,11 @@
+function Incometracker() {
+
+      
+
+    return (
+        <div className="Income-tracker">
+        <h3>Income Tracker...</h3>
+        </div>
+    );
+}
+export default Incometracker;
